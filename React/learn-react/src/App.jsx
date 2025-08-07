748a834a17830eadaec0b5633f27eb770ca907f7
PeasantTech <peasantpixels@gmail.com>
2025-08-07 13:55:36 -0400

    Attempted to get React via Vite working. Failed

---
+++ React/learn-react/src/App.jsx
@@ -0,0 +1,48 @@
+//import { loadConfigFromFile } from 'vite';
+import './App.css';
+import { useState } from "react";
+
+function Board() {
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    console.log(squares[i]);
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+    console.log(squares[i]);
+  }
+
+  return (
+    <div className="TicTacToe">
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+
+      </div>
+    </div>
+  );
+}
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button> 
+  );
+}
+
+export default Board;
