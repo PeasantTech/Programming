5d30170bb9ceb49f7ee7c3b51c189695b5c3b3b9
PeasantTech <peasantpixels@gmail.com>
2025-08-11 15:26:56 -0400

    Still learning JSON manipulation in js and HTML

--- HTML/JSONManip/testing.js
+++ HTML/JSONManip/testing.js
@@ -1,34 +1,53 @@
-// const fs = require('fs');
-// let fileText = fs.readFileSync('http://127.0.0.1:3000/HTML/JSONManip/JSON/Objects.json');
-// let objectData = JSON.parse(fileText);
-
-const url = "http://127.0.0.1:3000/HTML/JSONManip/JSON/Objects.json";
-const objectData = await fetch(url);
-
-
-const searchKey = "Name";
-const searchVal = document.getElementById("name-input");
+const searchValInput = document.getElementById("search-value-input");
+const searchKeyInput = document.getElementById("search-key-input");
+const keyValSearch = document.getElementById("key-value-search");
+const objIDSearch = document.getElementById("object-id-search");
+const keyValBtn = document.getElementById("key-value-search-button");
+const objIDBtn = document.getElementById("object-id-search-button");
 const submitBtn = document.getElementById("submit-button");
+
+let objectData = null;
+
+async function getData() {
+  const url = "http://127.0.0.1:3000/HTML/JSONManip/JSON/Objects.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    objectData = await response.json();
+    console.log(objectData);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
 const findObject = (key, value, parent) => {
     for (const objKey in parent) {
         if (parent[objKey][key] === value) {
-            return parent[objKey];
+            parent[objKey];
         }
     }
     return null;
 };
 
 const searchObject = () => {
+    const searchVal = searchValInput.value;
+    const searchKey = searchKeyInput.value;
     const result = findObject(searchKey, searchVal, objectData);
-    console.log(result);
+    console.log("result", result);
+    
+    // console.log(searchValInput, searchValInput.value, searchVal);
+    // console.log("searchKey", searchKey);
+    // console.log("searchVal", searchVal);
 }
 
-//submitBtn.addEventListener("click", searchObject);
+const displayKeyVal = () => {keyValSearch.classList.remove("hide"); objIDSearch.classList.add("hide")};
+const displayObjId = () => {keyValSearch.classList.add("hide"); objIDSearch.classList.remove("hide")};
 
+keyValBtn.addEventListener("click", displayKeyVal);
+objIDBtn.addEventListener("click", displayObjId);
 
-const logToConsole = () => {
-    console.log("click!");
-}
-
-submitBtn.addEventListener("click", logToConsole);
+submitBtn.addEventListener("click", searchObject);
+window.addEventListener("load", getData);
