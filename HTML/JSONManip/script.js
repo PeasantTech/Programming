245ecf1d2f8ab257f14cfb34899a1534d458f607
PeasantTech <peasantpixels@gmail.com>
2025-08-19 15:57:17 -0400

    i dont remember

--- HTML/JSONManip/script.js
+++ HTML/JSONManip/script.js
@@ -1,13 +1,34 @@
-const person1 = {name : "John", age : 40};
-const person2 = {name : "Jane", age : 35};
-const person3 = {name : "Jake", age : 30};
-const person4 = {name : "Johan", age : 37};
-const person5 = {name : "Jared", age : 32};
+const person1 = { name: "John", age: 40 };
+const person2 = { name: "Jane", age: 35 };
+const person3 = { name: "Jake", age: 30 };
+const person4 = { name: "Johan", age: 37 };
+const person5 = { name: "Jared", age: 32 };
 
-const personObj = {person1, person2, person3, person4, person5};
+const array = [1, 2, 3, 4, 5];
 
-console.log(personObj);
+// console.log(array.slice(-2));
+
+const personObj = { person1, person2, person3, person4, person5 };
+
+// console.log(personObj);
 
 for (const objKey in personObj) {
-    console.log(personObj[objKey], objKey);
+    // console.log(personObj[objKey], objKey);
 }
+
+const randoFunc = nums => Math.floor(Math.random() * (nums[1] - nums[0]) + nums[1]);
+
+// console.log(randoFunc(array));
+
+const range = (x,y) => {
+    for (const index in array) {
+        console.log(array[index]);
+        console.log(array[0]);
+        if (array[index]+x > x && array[index]+x < y) {
+            console.log(array[index]);
+            array[index] = array[index - 1] + 1
+        }
+    }
+}
+
+range(4,8);
